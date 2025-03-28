b31d2214738672c4743e5634d0d209474ff43643
felipegonzaleze <fgonech@gmail.com>
2025-03-28 05:49:12 -0300

    Función changeTempunit() añadida

--- tiempoLocal/script/script.js
+++ tiempoLocal/script/script.js
@@ -1,7 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
+    changeTempUnit();
     removeCookies();
 })
 
+let actualUnit = "Celsius"
+function changeTempUnit() {
+    let unit = document.getElementById("temp-unit");
+    let temperature = document.querySelectorAll(".weather-temperature");
+    unit.addEventListener("change", function() {
+        let selectedUnit = this.value;
+        if (actualUnit == "Celsius" && selectedUnit == "Fahrenheit") {
+            temperature.forEach(function(temp){
+                let maxValue = parseInt(temp.querySelector(".unitMax").textContent);
+                let minValue = parseInt(temp.querySelector(".unitMin").textContent);
+                let maxNewValue = Math.round((maxValue * 9/5) + 32);
+                let minNewValue = Math.round((minValue * 9/5) + 32);
+                temp.innerHTML = `<span class="unitMax">${maxNewValue}</span>°/<span class="unitMin">${minNewValue}</span>°`
+            })
+        }
+        else if (actualUnit == "Fahrenheit" && selectedUnit == "Celsius") {
+            temperature.forEach(function(temp){
+                let maxValue = parseInt(temp.querySelector(".unitMax").textContent);
+                let minValue = parseInt(temp.querySelector(".unitMin").textContent);
+                let maxNewValue = Math.round((maxValue - 32) * 5/9);
+                let minNewValue = Math.round((minValue - 32) * 5/9);
+                temp.innerHTML = `<span class="unitMax">${maxNewValue}</span>°/<span class="unitMin">${minNewValue}</span>°`
+            })
+        }
+        actualUnit = selectedUnit;
+    })
+}
 function removeCookies() {
     let acceptButton = document.querySelector(".accept-btn");
     let cookies = document.querySelector(".cookies");
