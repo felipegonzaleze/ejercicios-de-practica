e5ab2192508a7eeae929860420779e5bc042a44d
felipegonzaleze <fgonech@gmail.com>
2025-03-31 18:05:31 -0300

    Añadido ejercicio calculadora

---
+++ calculadora/script.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const display = document.getElementById("display");
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", handleClick);
+  });
+
+  handleClick();
+  appendToDisplay();
+  clearDisplay();
+  deleteLastChar();
+  calculate();
+});
+
+// Function to handle button click
+function handleClick(event) {
+  const buttonValue = event.target.innerText;
+  switch (buttonValue) {
+    case "C":
+      clearDisplay();
+      break;
+    case "DEL":
+      deleteLastChar();
+      break;
+    case "=":
+      calculate();
+      break;
+    default:
+      appendToDisplay(buttonValue);
+  }
+}
+
+// Function to append value to display
+function appendToDisplay(value) {
+  display.value += value;
+}
+
+// Function to convert C in DEL
+function checkValue() {
+}
+// Function to clear display
+function clearDisplay() {
+  display.value = "";
+}
+
+// Function to delete last character
+function deleteLastChar() {
+  display.value = display.value.slice(0, -1);
+}
+
+// Function to calculate result
+function calculate() {
+  try {
+    const result = eval(display.value);
+    display.value = result;
+  } catch (error) {
+    display.value = "Error";
+  }
+}
